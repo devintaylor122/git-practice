76bd86f1f873389f74655cf185fea805b9b54e48
Devin Taylor <devin.taylor122@gmail.com>
2022-09-22 13:40:46 -0700

    finished function for function b

--- function_b.py
+++ function_b.py
@@ -7,7 +7,16 @@ def silly_sum():
         the user enters 0, or
         the sum reaches or exceeds 1000
     """
-    pass
+    sum = 0
+    while True:
+        input = input_int()
+        if input == 0:
+            break
+        if sum >= 1000:
+            break
+        else:
+            sum += input
+    return sum
 
 
 if __name__ == "__main__":
